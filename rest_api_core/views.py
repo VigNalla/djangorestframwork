db0a892ae31034ee77d7230d1ad6bc2b6c6cfa4a
vigneshtnv@outlook.com <vigneshtnv@outlook.com>
2025-08-09 11:27:12 +0530

    changed views day 1

--- rest_api_core/views.py
+++ rest_api_core/views.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 
 class HelloAPIView(APIView):
     def get(self, request):
-        return Response({"message": "Hello, API apprentice!!!"})
+        return Response({"success":True, "message": "Hello, API apprentice!!!"})
     
     def post(self, request):
         name = request.data.get('name')
         message = request.data.get('message')
-        return Response({"message": f"Hello, {name} \n {message}"})
+        if not name:
+            return Response({"success":False, "message": "Name is required!" })
+        return Response({"success":True, "message": f"Hello, {name} \n {message}"})
